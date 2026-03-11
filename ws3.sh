1c779cd321d89744600bb771e0f2e0cacab47d52
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-03-11 22:30:10 +0800

    final

--- ws3.sh
+++ ws3.sh
@@ -1,15 +1,32 @@
+# #!/bin/bash
+
+# acccode=1234
+# read -p 'What is your access code?: ' usrcde
+
+# if [ $USER = 'root' ]; then
+#    echo "Access granted - full privileges"
+# elif [ $usrcde = $acccode ]; then
+# echo "Access granted - limited privileges"
+# else
+# echo "Access denied"
+#       exit 1
+# fi
+
+# exit 0
+
+#Exercise 2
+
 #!/bin/bash
+read -p "Please enter an integer between 30 and 40 inclusive: " int
 
-acccode=1234
-read -p 'What is your access code?: ' usrcde
+if [[ $int -ge 30 ]] && [[ $int -le 40 ]]; then
 
-if [ $USER = 'root' ]; then
-   echo "Access granted - full privileges"
-elif [ $usrcde = $acccode ]; then
-echo "Access granted - limited privileges"
+    echo "$int is a valid selection"
+
 else
-echo "Access denied"
-      exit 1
+    echo "$int is not a valid selection"
+
 fi
 
 exit 0
+
