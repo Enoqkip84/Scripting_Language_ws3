46791d3e191a671aa218633e2849029172d6f700
Enoqkip84 <enoq.kipchirchir@gmail.com>
2026-03-11 22:13:08 +0800

    files

--- ws3.sh
+++ ws3.sh
@@ -3,18 +3,13 @@
 acccode=1234
 read -p 'What is your access code?: ' usrcde
 
-if [ $user = 'root' ]; then
-
+if [ $USER = 'root' ]; then
    echo "Access granted - full privileges"
-elif [ $usrcde=$acccode ]; then
-
+elif [ $usrcde = $acccode ]; then
 echo "Access granted - limited privileges"
-
 else
-
 echo "Access denied"
       exit 1
-
 fi
 
 exit 0
